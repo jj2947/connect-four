ff5761d20895990b9f9219cf31f2d35ceea6c0af
Jessica Jiang <jessicaj2947@gmail.com>
2023-05-03 16:45:09 +1200

    Added game loop

--- Main.java
+++ Main.java
@@ -1,8 +1,39 @@
 public class Main {
 
-    public static void main(String[] args) {
-        
-    }
+  public static void main(String[] args) {
 
-    
+    BoardArray boardArray = new BoardArray(new char[7][8], 8, 7);
+
+    boardArray.initialiseBoard();
+    boardArray.printBoard();
+
+    int p1Win = 0;
+    int p2Win = 0;
+
+    /* Main game loop */
+
+    // While neither player has won, continue the game
+    while (p1Win == 0 && p2Win == 0) {
+      // Player 1's turn
+      Move move1 = new Move(1, boardArray);
+      move1.makeMove(move1.getMove());
+      boardArray.printBoard();
+
+      // Checks if player 1 has won and assigns the value to p1Win
+      p1Win = move1.CheckWin();
+
+      // If player 1 has won, break the while loop
+      if (p1Win == 1) {
+        break;
+      }
+
+      // Player 2's turn
+      Move move2 = new Move(2, boardArray);
+      move2.makeMove(move2.getMove());
+      boardArray.printBoard();
+
+      // Checks if player 2 has won and assigns the value to p2Win
+      p2Win = move2.CheckWin();
+    }
+  }
 }
