3e6c350b8938f7ca9ae61d0c752925ec645804c5
Jessica Jiang <jessicaj2947@gmail.com>
2023-05-03 16:33:45 +1200

    added checkwin method

--- Move.java
+++ Move.java
@@ -73,4 +73,135 @@ public class Move {
       }
     }
   }
+
+  // Function that checks if the player has won
+  // Function that checks if the player has won
+  public int CheckWin() {
+    int count = 0;
+    char symbol;
+    int row;
+    int col;
+    int win = 0;
+
+    // Assigns the correct symbol for each player
+    if (player == 1) {
+      symbol = 'X';
+    } else {
+      symbol = 'O';
+    }
+
+    // Check for vertical win
+
+    for (col = 1; col < boardArray.getWidth() - 1; col++) {
+      count = 0;
+      for (row = boardArray.getHeight() - 1; row >= 0; row--) {
+        // If the symbol is the same as the player's symbol, increment count
+        if (boardArray.getBoard()[row][col] == symbol) {
+          count++;
+
+          if (count == 4) {
+            win = 1;
+          }
+        }
+
+        // If the symbol is not the same as the player's symbol, reset count and continue the loop
+        else if (boardArray.getBoard()[row][col] != symbol && count >= 1) {
+          count = 0;
+        }
+      }
+    }
+
+    // Check for horizontal win
+    for (row = boardArray.getHeight() - 1; row >= 0; row--) {
+      count = 0;
+      for (col = 1; col < boardArray.getWidth() - 1; col++) {
+        // If the symbol is the same as the player's symbol, increment count
+        if (boardArray.getBoard()[row][col] == symbol) {
+          count++;
+
+          if (count == 4) {
+            win = 1;
+          }
+        }
+
+        // If the symbol is not the same as the player's symbol, reset count and continue the loop
+        else if (boardArray.getBoard()[row][col] != symbol && count >= 1) {
+          count = 0;
+        }
+      }
+    }
+
+    // Check for left diagonal win
+    int maxRow, maxCol;
+
+    for (row = 0; row < boardArray.getHeight(); row++) {
+      for (col = 0; col < boardArray.getWidth(); col++) {
+        // Set the maximum row and column to check that they will be within the boardArray bounds
+        maxRow = row - 3;
+        maxCol = col + 3;
+
+        // If the maximum row and column are within the boardArray bounds and there are 4 symbols in
+        // a left diagonal, the player has won
+        if (maxRow >= 0
+            && maxCol < boardArray.getWidth()
+            && boardArray.getBoard()[row][col] == symbol
+            && boardArray.getBoard()[row - 1][col + 1] == symbol
+            && boardArray.getBoard()[row - 2][col + 2] == symbol
+            && boardArray.getBoard()[row - 3][col + 3] == symbol) {
+          win = 1;
+        }
+      }
+    }
+
+    // Check for right diagonal win
+    for (row = 0; row < boardArray.getHeight(); row++) {
+      for (col = 0; col < boardArray.getWidth(); col++) {
+        // Set the maximum row and column to check that they will be within the boardArray bounds
+        maxRow = row - 3;
+        maxCol = col - 3;
+
+        // If the maximum row and column are within the boardArray bounds and there are 4 symbols in
+        // a right diagonal, the player has won
+        if (maxRow >= 0
+            && maxCol >= 0
+            && boardArray.getBoard()[row][col] == symbol
+            && boardArray.getBoard()[row - 1][col - 1] == symbol
+            && boardArray.getBoard()[row - 2][col - 2] == symbol
+            && boardArray.getBoard()[row - 3][col - 3] == symbol) {
+          win = 1;
+        }
+      }
+    }
+
+    // If the player has won, print the player that won and return 1
+    if (win == 1) {
+      if (player == 1) {
+        System.out.println("\n\nPlayer 1 Wins!!\n\n");
+        return 1;
+      } else {
+        System.out.println("\n\nPlayer 2 Wins!!\n\n");
+        return 1;
+      }
+    }
+
+    // Check for tie, when the boardArray is full and neither player has won
+    for (int i = 0; i < boardArray.getHeight(); i++) {
+      for (int j = 0; j < boardArray.getWidth(); j++) {
+        // If the board is not full, break the loop and the function will return 0
+        if (boardArray.getBoard()[i][j] == ' ') {
+          i = boardArray.getHeight();
+          break;
+        }
+
+        // If the board is full and neither player has won, print tie and return 1
+        else if (i == boardArray.getHeight() - 1 && j == boardArray.getWidth() - 1) {
+          System.out.println("\n\nTie!!\n\n");
+          return 1;
+        }
+      }
+    }
+
+    // If the player has not won, return 0
+    return 0;
+  }
 }
